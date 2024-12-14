c3a6964912237d6372e1f85dfd9d542c8c36d87e
Tayab Raza <tayab@sm.com>
2024-12-14 13:17:14 +0500

    Initial commit

---
+++ app/lib/tai_definitions.ts
@@ -0,0 +1,8 @@
+export type User = {
+    user_id:number;
+    user_name:string;
+};
+export type Customer = {
+    id:number;
+    name:string;
+};
---
+++ app/lib/tai_p;aceholder.ts
@@ -0,0 +1,16 @@
+const users = [
+    {
+        user_id:1,
+        user_name:"tayab"
+    },
+    {
+        user_id:2,
+        user_name:"hamid"
+    }
+];
+const customers = [
+
+];
+const invoices = [
+
+];
